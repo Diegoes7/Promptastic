e49360aef70263c50c57fc39e1257edafa31c6cd
DiegoElRey <d.dimitrov@bpmhub.com>
2024-12-19 19:58:15 +0200

    remove the migration file

--- app/api/db/supabase_client.ts
+++ app/api/db/supabase_client.ts
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Key are required');
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
