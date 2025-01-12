0b415ae558b0aa1e4ed44176508a8686a0f1c5aa
DiegoElRey <d.dimitrov@bpmhub.com>
2025-01-12 14:43:46 +0200

    ch

--- app/api/db/typeorm.config.ts
+++ app/api/db/typeorm.config.ts
@@ -17,7 +17,7 @@ export const AppDataSource = new DataSource({
   entities: [Favorite, User, Prompt, Picture],
   // relationLoadStrategy: "query", // Ensures relations are lazy-loaded with queries
   // synchronize: process.env.NODE_ENV === "development",
-  synchronize: false,
+  synchronize: true,
   logging: process.env.NODE_ENV === "development",
   // migrations: ['./migrations/*.sql'],
   subscribers: [],
